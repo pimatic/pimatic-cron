63cd15e2dbb2863e73430f668f27986c724af9f1
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2014-01-08 07:50:58 +0100

    generated doc files

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -85,16 +85,16 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
-            "title": "The ClockBackend",
-            "slug": "the-clockbackend"
+            "title": "The CronPlugin",
+            "slug": "the-cronplugin"
           },
           "depth": 2
         }, {
           "type": "heading",
           "data": {
             "level": 2,
-            "title": "The Clock-Actuator",
-            "slug": "the-clock-actuator"
+            "title": "The PredicateProvider",
+            "slug": "the-predicateprovider"
           },
           "depth": 2
         }
